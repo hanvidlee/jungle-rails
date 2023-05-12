d77e85bc7447bcbb8a55634da3466d0c31abaa82
hanvidlee <hanvidlee@gmail.com>
2023-05-11 21:50:02 -0400

    cypress test for add to cart

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,10 @@
+describe("Add to cart", () => {
+
+  it("it should visit the home page", () => {
+    cy.visit("/");
+  });
+  it('clicks on add button on one of the products', () => {
+    cy.get('.btn').contains("Add").click({ force: true });
+    cy.get(".nav-item").contains("My Cart (1)").should("be.visible");
+  });
+})
